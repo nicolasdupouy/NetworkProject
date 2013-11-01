2ccaec8f205250c25b255766592ae1f7077e4c71
Nicolas Dupouy <nicolasdupouy@gmail.com>
2013-11-01 13:36:24 +0100

    Change constants interfaces to final classes.

--- src/main/java/com/ndu/client/constants/ClientConstants.java
+++ src/main/java/com/ndu/client/constants/ClientConstants.java
@@ -1,34 +1,38 @@
 package com.ndu.client.constants;
 
-public interface ClientConstants {
+public final class ClientConstants {
 
+	private ClientConstants() {
+		
+	}
+	
 	/*
 	 * ##########################
 	 * ## Connection on server ##
 	 * ##########################
 	 */
-	public static String CONNECTION_GRANTED				= "Connection granted.\n\n";
-	public static String CONNECTION_REFUSED				= "Connection refused.\n\n";
+	public static final String CONNECTION_GRANTED				= "Connection granted.\n\n";
+	public static final String CONNECTION_REFUSED				= "Connection refused.\n\n";
 	
 	/*
 	 * #################################
 	 * ## CommunicationChannel prompt ##
 	 * #################################
 	 */
-	public static String COMMUNICATION_CHANNEL_PROMPT	= "CLIENT SIDE";
+	public static final String COMMUNICATION_CHANNEL_PROMPT	= "CLIENT SIDE";
 	
 	/*
 	 * ################
 	 * ## Exceptions ##
 	 * ################
 	 */
-	public static String EXCEPTION_READ_TERMINAL		= "Exception while reading from terminal";
-	public static String EXCEPTION_WRITE_TERMINAL		= "Exception while writing to terminal";
-	public static String EXCEPTION_READ_CORRESPONDANT	= "Exception while reading from correspondant";
-	public static String EXCEPTION_WRITE_CORRESPONDANT	= "Exception while writing to correspondant";
+	public static final String EXCEPTION_READ_TERMINAL		= "Exception while reading from terminal";
+	public static final String EXCEPTION_WRITE_TERMINAL		= "Exception while writing to terminal";
+	public static final String EXCEPTION_READ_CORRESPONDANT	= "Exception while reading from correspondant";
+	public static final String EXCEPTION_WRITE_CORRESPONDANT	= "Exception while writing to correspondant";
 	
-	public static String EXCEPTION_UNSUPPORTED_OPERATION	= "Unsupported operation !";
+	public static final String EXCEPTION_UNSUPPORTED_OPERATION	= "Unsupported operation !";
 	
-	public static String EXCEPTION_CONNECTION_ENDED_SUCCESS	= "Connection ended with SUCCESS";
-	public static String EXCEPTION_CONNECTION_ENDED_ERROR	= "Connection ended with ERROR";
+	public static final String EXCEPTION_CONNECTION_ENDED_SUCCESS	= "Connection ended with SUCCESS";
+	public static final String EXCEPTION_CONNECTION_ENDED_ERROR	= "Connection ended with ERROR";
 }
